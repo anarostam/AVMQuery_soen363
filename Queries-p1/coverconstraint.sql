100b832cd77cb35e4d81ac32c5f66ca900bc196e
manelhellou <manelhellou0503@outlook.fr>
2025-04-13 19:38:53 -0400

    Update coverconstraint.sql

--- Queries-p1/coverconstraint.sql
+++ Queries-p1/coverconstraint.sql
@@ -1,12 +1,12 @@
 --8)overlap constraints
-SELECT p.ssn, p.first_name
+SELECT p."SSN"
 FROM person p
-JOIN Victim v ON p.SSN = v.ssn
-JOIN emergency_respondent er ON p.SSN = er.ssn
+JOIN Victim v ON p."SSN" = v.ssn
+JOIN emergency_respondent er ON p."SSN" = er.ssn
 --Covering constraints
-SELECT p.SSN, p.first_name
+SELECT p."SSN"
 FROM person p
-WHERE p.SSN NOT IN (
+WHERE p."SSN" NOT IN (
 SELECT ssn FROM Victim 
 UNION
 SELECT ssn FROM emergency_respondent
